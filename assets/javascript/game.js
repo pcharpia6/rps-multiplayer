b59969fba7e2273abb59638615c855997b3bbf0d
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-04-23 17:20:17 -0400

    moved all js to single file. simplifying the structure of the app for now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,43 @@
-// var config = {
-//     apiKey: "AIzaSyACRvX97cxEarRcZdnWENxquUL5u6TJlGw",
-//     authDomain: "rock-paper-scissors-fb335.firebaseapp.com",
-//     databaseURL: "https://rock-paper-scissors-fb335.firebaseio.com",
-//     projectId: "rock-paper-scissors-fb335",
-//     storageBucket: "rock-paper-scissors-fb335.appspot.com",
-//     messagingSenderId: "693363059042"
-//   };
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyACRvX97cxEarRcZdnWENxquUL5u6TJlGw",
+    authDomain: "rock-paper-scissors-fb335.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-fb335.firebaseio.com",
+    projectId: "rock-paper-scissors-fb335",
+    storageBucket: "rock-paper-scissors-fb335.appspot.com",
+    messagingSenderId: "693363059042"
+  };
+firebase.initializeApp(config);
+
+// function submitCreateName() {
+//     var displayName = document.querySelector("entry-displayname");
+
+//     firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//     .then(function(user) {
+//         //add the displayName
+//         user.updateProfile({displayName: displayName.value});
+//     });
+// }
+
+// function signInWithEmailandPassword() {
+//     var email = document.querySelector("#email");
+//     var password = document.querySelector("#password");
+
+//     firebase.auth().signInWithEmailandPassword(email.value, password.value);
+// }
+
+// function authStateChangeListener(user) {
+//     //signin
+//     if (user) {
+//         Chat.onlogin();
+//         Game.onlogin();
+//     } else { //signout
+//         window.location.reload();
+//     }
+// }
+
+// $("#login").on("click", function() {
+
+// })
 
 // //setting up open/closed games
 // var STATE = {OPEN: 1, JOINED: 2, CHOOSE_RPS: 3, RPS_PICKED: 4, COMPLETE: 5};
